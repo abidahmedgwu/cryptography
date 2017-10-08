68cb89095078ce01db7a1c30328b0f94ad02750a
abidahmedgwu <abidahmed@gwu.edu>
2017-10-08 15:21:55 -0400

    Fixed sizing error on DoubleFrame on smaller screens.

--- src/GeneralFrame.java
+++ src/GeneralFrame.java
@@ -13,7 +13,7 @@ public class GeneralFrame extends JFrame
     // Row 1
     protected JPanel row1 = new JPanel();
     protected JLabel inputLabel = new JLabel("Input: ");
-    protected JTextArea input = new JTextArea(4, 65);
+    protected JTextArea input = new JTextArea(6, 100);
     protected JScrollPane inputScroll = new JScrollPane(input, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
                                     JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
     // Row 2 
@@ -28,18 +28,18 @@ public class GeneralFrame extends JFrame
     // Row 4
     protected JPanel row4 = new JPanel();
     protected JLabel outputLabel = new JLabel("Output: ", JLabel.LEADING);
-    protected JTextArea output = new JTextArea(4, 65);
+    protected JTextArea output = new JTextArea(6, 100);
     JScrollPane outputScroll = new JScrollPane(output, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
                                     JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
     // Layouts
-    protected FlowLayout layout1 = new FlowLayout(FlowLayout.CENTER);
+    protected FlowLayout layout1 = new FlowLayout(FlowLayout.CENTER, 5, 30);
     protected FlowLayout layout2 = new FlowLayout(FlowLayout.CENTER, 5, 45);
     protected  FlowLayout layout3 = new FlowLayout(FlowLayout.CENTER, 5, -1);
     protected FlowLayout layout4 = new FlowLayout(FlowLayout.CENTER, 5, 15);
     
     public GeneralFrame() {
         super();
-        setSize(650, 500);
+        setSize(1000, 750);
         setLocationRelativeTo(null);
         setLookAndFeel();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
